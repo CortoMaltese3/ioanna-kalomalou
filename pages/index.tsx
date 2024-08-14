79d05c39cad9797077a2e08e90969f5d7e632d97
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-08-14 17:01:21 +0300

    chore/Remove unnecessary code comments and not used images

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,6 @@ const About: NextPage = () => {
         </h4>
 
         <div className="grid gap-6 my-3 md:grid-cols-2">
-          {/* children's initial and animate property should be same as the parent during a stagger effect  */}
           {services.map((service) => (
             <div
               className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-dark-200 md:col-span-1 "
@@ -32,31 +31,5 @@ const About: NextPage = () => {
     </div>
   );
 };
-
-//!called every time  the page refreshed
-// export const getServerSideProps: GetServerSideProps = async (
-//    context: GetServerSidePropsContext
-// ) => {
-//    const res = await fetch('http://localhost:3000/api/services')
-//    const data = await res.json()
-//    console.log(data)
-//    return { props: { services: data.services } }
-// }
-
-//!called only during the build of the project
-//? make sure the server(localhost:3000)[this will receive the request during build] is running on a terminal during the build
-//? also need to change the localhost during the deployment | see the todo
-// https://aude53.medium.com/set-environment-variables-with-next-js-and-vercel-e544c0460a48
-
-// export const getStaticProps: GetStaticProps = async (
-//    context: GetStaticPropsContext
-// ) => {
-//    // console.log(context);
-
-//    const res = await fetch('http://localhost:3000/api/services')
-//    const { services } = await res.json()
-//    console.log({ services })
-//    return { props: { services: services } }
-// }
 
 export default About;
